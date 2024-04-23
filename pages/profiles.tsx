95eb53cdaf72bffae783d4bfc76187517779a913
jluncashu1 <167792468+jluncashu1@users.noreply.github.com>
2024-04-23 08:36:49 +0300

    Update profiles.tsx

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -26,42 +26,42 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 }
 
 const Profiles = () => {
-    const { data: user } = useCurrentUser();
-    const router = useRouter();
+  const router = useRouter();
+  const { data: user } = useCurrentUser();
 
-    return (
-        <div className="flex items-center h-full justify-center">
-            <div className="flex flex-col">
-                <h1 className="text-3xl md:text-6xl text-white text-center">
-                    Who is watching?
-                </h1>
-                <div className="flex items-center justify-center gap-8 mt-10">
-                    <div onClick={() => {router.push('/')}}>
-                        <div className="group flex-row w-44 mx-auto">
-                            <div className="
-                                w-44
-                                h-44
-                                rounded-md
-                                flex
-                                items-center
-                                justify-center
-                                border-2
-                                border-transparent
-                                group-hover:cursor-pointer
-                                group-hover:border-white
-                                overflow-hidden
-                            ">
-                                <Image src='/images/default-green.png' width={200} height={200} alt="Profile" />
-                            </div>
-                            <div className="mt-4 text-gray-400 text-2xl text-center group-hover:text-white">
-                                {user?.name}
-                            </div>
-                        </div>
-                    </div>
+  const selectProfile = useCallback(() => {
+    router.push("/");
+  }, [router]);
+
+  return (
+    <>
+      <Head>
+        <title>Profiles | Letflix</title>
+        <meta name="description" content="Letflix, Profiles page" />
+      </Head>
+
+      <div className="flex items-center h-full justify-center">
+        <div className="flex flex-col">
+          <h1 className=" text-3xl md:text-6xl text-white text-center">
+            Who is watching?
+          </h1>
+          <div className="flex items-center justify-center gap-8 mt-10">
+            <div onClick={selectProfile}>
+              <div className=" group flex-row w-44 mx-auto">
+                <div className="w-44 h-44 rounded-md flex justify-center items-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden">
+                  <img src="/images/default-green.png" alt="profile" />
                 </div>
+
+                <div className=" mt-4 text-gray-400 text-2xl text-center group-hover:text-white">
+                  {user?.name}
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default Profiles;
